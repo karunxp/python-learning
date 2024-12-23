c51bd167be6e065086c8b526a670a4d30506815e
Arun Kumar <karunxp@gmail.com>
2024-12-24 00:09:24 +0530

    chapter-1

---
+++ Chapter-1/strings.py
@@ -0,0 +1,13 @@
+# strings
+# collections of charactor inside single quotes or double quotes
+
+first_name = "Shivangi"
+last_name =  "Singh"
+full_name = first_name + " " + last_name   #space addition usingh doble qoutes
+print(full_name)
+
+#print(first_name + 3) # error
+print(first_name + "3") # no error
+print(first_name + str(3)) # no error
+
+print(first_name * 5) # multiplication will work that will print last_name 5 times
