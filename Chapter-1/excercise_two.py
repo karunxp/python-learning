a23a1f3e6e6c529e80457d6cdf8d97f007ed3f6a
Arun Kumar <karunxp@gmail.com>
2024-12-24 23:43:54 +0530

    Create excercise_two.py

---
+++ Chapter-1/excercise_two.py
@@ -0,0 +1,19 @@
+#number1 = int(input("Enter the values1: "))
+#number2 = int(input("Enter the values2: "))
+#number3 = int(input("Enter the values3: "))
+
+#total = number1 + number2 + number3
+#print(total)
+
+#print(f"Total average is {total//3}")
+
+#num1 = input("Enter the value1: ")
+#num2 = input("Enter the value2: ")
+#num3 = input("Enter the value3: ")
+
+#(int(num1) + int(num2) + int(num3)) /3
+
+#####single line input#####
+num1, num2, num3 = input("Enter the all three values ").split()
+
+print(f"Average of three numbers is {(int(num1) + int(num2) + int(num3)) /3}")
