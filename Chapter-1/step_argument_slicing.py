ee490cf0cfa45cfffd6e35795951c914f1299ef8
Arun Kumar <karunxp@gmail.com>
2024-12-25 00:35:46 +0530

    chapter-1-complete

---
+++ Chapter-1/step_argument_slicing.py
@@ -0,0 +1,14 @@
+# slicing / slecting sub sequences
+
+#lang = "Python"
+# syntax - [ start argument : stop argument -1 : step]
+
+print("ArunKumar"[0:5])
+
+print("ArunKumar"[0:5:2]) # step argument
+print("ArunKumar"[0::2]) # step argument
+
+print("ArunKumar"[5::-1]) # step argument
+
+print("ArunKumar"[-1::-1]) # reverse string
+print("ArunKumar"[::-1]) # reverse string
---
+++ Chapter-1/string_slicing.py
@@ -0,0 +1,16 @@
+# slicing / slecting sub sequences
+
+lang = "Python"
+# print(lang[4])
+# syntax - [ start argument : stop argument -1]
+
+print(lang[2:4])
+
+#print(lang[-2:4]) # reverse index printing
+
+print(lang[2:]) # second character bas sara print ho jayega
+
+print(lang[:3]) # print character from start to 3rd charactor
+
+
+
