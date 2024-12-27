1ae7abea5f10819c98d0f931087f699a21c7f93e
Arun Kumar <karunxp@gmail.com>
2024-12-27 06:46:07 +0530

    Chapter-3 if else condition

---
+++ Chapter-3/else_statement.py
@@ -0,0 +1,9 @@
+
+age = input("enter your age: ")
+age = int(age)
+
+if age >= 14:
+    print("line a")
+    print("you are above 14")
+else:
+    print("you age is not qualify to play the game.")
---
+++ Chapter-3/pass.py
@@ -0,0 +1,8 @@
+# pass statement
+
+x = 18
+if x >18
+    pass
+
+# pass ka matlab ab iske koi ye condition aage nahi run hoga, same break
+
