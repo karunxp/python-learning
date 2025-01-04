f3f8760e3d062b4815e6f9e7918b87a5bca32bb5
Arun Kumar <karunxp@gmail.com>
2025-01-05 00:21:25 +0530

    rgegr

---
+++ Chapter-3/for_loop_strings.py
@@ -0,0 +1,13 @@
+#name = "arun"
+#for x in range(len(name)):
+#    print(name[x])
+
+name = "Arun"
+for x in name:
+    print(x) 
+
+num = input("enter a number : ")
+total = 0
+for x in num:
+    total += int(x)
+    print(total)    
---
+++ Chapter-3/step.py
@@ -0,0 +1,21 @@
+# step agrgument with for loop
+
+for x in range(1,10,2):
+    print(x)
+
+###even numbers
+for y in range(0,10,2):
+    print(y)   
+
+# 10 , 9 , 8
+#reverse order with step argument
+for i in range(10,0,-1):
+    print(i)
+  
+for i in range(10,-1,-1):
+    print(i)
+
+
+for i in range(1,-11,-1):
+    print(i)
+
